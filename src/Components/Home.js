d4a696f9b188892684feded9a69e60b14fa47cc6
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2024-11-01 23:26:30 +0530

    home page slight change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { HomeWrapper } from "./Container";
-
 const Home = () => {
   return (
     <HomeWrapper id="home">
       <h1>
-        Hi, I'm <span className="name-text">Vighnesh</span>
+        Hey, I'm <span className="name-text">Vighnesh</span>
         <span className="cursor">|</span>
       </h1>
-      <p className="intro-text">I make things. Sometimes, they even work.</p>
+      <p className="intro-text">
+        Crafting digital experiences with code & creativity.
+      </p>
       <p className="bio-text">
-        Based in India, I'm passionate about design, art, and software
-        engineering. I love creating innovative solutions that make a
-        difference.
+        Based in India, I’m a software engineer with a flair for design and
+        innovation. I build solutions that solve problems and bring ideas to
+        life.
       </p>
     </HomeWrapper>
   );
