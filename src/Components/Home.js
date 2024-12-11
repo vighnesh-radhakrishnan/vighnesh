fdb69a6143191f70d70b0a5f0be68f762e66d2da
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2024-12-11 23:37:08 +0530

    minor text correction

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       </h1>
       <p className="intro-text">I make things. Sometimes, they even work.</p>
       <p className="bio-text">
-        Based in India, I’m a software engineer with over three years of
+        Based in India, I’m a Software Developer with over three years of
         experience with a flair for design and innovation. I build solutions
         that tackle real challenges and turn ideas into reality.
       </p>
