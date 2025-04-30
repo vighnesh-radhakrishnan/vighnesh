c18b2abab815e298f1f42955058385f7542cf57e
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2025-05-01 03:30:30 +0530

    reverted and added new change

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ import Projects from "./Components/Projects";
 import Footer from "./Components/Footer";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
-import {
-  GradientBackground,
-  EnhancedAuroraBackground,
-  NoiseOverlay,
-} from "./Components/Container";
+import { GradientBackground } from "./Components/Container";
 
 const App = () => (
   <>
     <Navbar />
-    <EnhancedAuroraBackground />
-    <NoiseOverlay />
+    <GradientBackground />
 
     <Router>
       <div>
